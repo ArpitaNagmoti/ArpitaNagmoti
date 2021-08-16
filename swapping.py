222999fe051406681869e58875265a32f1d8922d
ArpitaNagmoti <88833104+ArpitaNagmoti@users.noreply.github.com>
2021-08-16 12:45:13 +0530

    Add files via upload

---
+++ swapping.py
@@ -0,0 +1,12 @@
+#Swap two numbers using temporary variable
+x = 30
+y = 10
+
+temp = x
+x = y
+y = temp
+
+print("The value of x after swapping: ",(x))
+print("The value of y after swapping: ",(y))
+
+  
